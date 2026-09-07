15576592e9c6012cf7a5ec20a75c00a415fb81f5
Artem Khrustalev <khru-artem@yandex.ru>
2026-09-07 22:54:13 +0300

    fix:ошибки сборки

--- src/data/photos.ts
+++ src/data/photos.ts
@@ -1,10 +1,8 @@
-import type { ImageMetadata } from "astro";
-
-import photoSource1 from "../assets/images/gallery/photo_1.jpg";
-import photoSource2 from "../assets/images/gallery/photo_2.jpg";
-import photoSource3 from "../assets/images/gallery/photo_3.jpg";
-import photoSource4 from "../assets/images/gallery/photo_4.jpg";
-import photoSource5 from "../assets/images/gallery/photo_5.jpg";
+import photoSource1 from "../assets/images/gallery/photo_1.png";
+import photoSource2 from "../assets/images/gallery/photo_2.png";
+import photoSource3 from "../assets/images/gallery/photo_3.png";
+import photoSource4 from "../assets/images/gallery/photo_4.png";
+import photoSource5 from "../assets/images/gallery/photo_5.png";
 
 export type PhotoItem = {
   src: string;
