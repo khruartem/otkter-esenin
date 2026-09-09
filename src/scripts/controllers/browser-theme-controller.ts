daddb5bd6b19ba336e301a935a298a0ac6856389
Artem Khrustalev <khru-artem@yandex.ru>
2026-09-09 22:03:54 +0300

    test:тестирование SCROLL_THRESHOLD = 40 контроллера browser-theme-bar

--- src/scripts/controllers/browser-theme-controller.ts
+++ src/scripts/controllers/browser-theme-controller.ts
@@ -1,5 +1,5 @@
 export class BrowserThemeController {
-  private static readonly SCROLL_THRESHOLD = 25;
+  private static readonly SCROLL_THRESHOLD = 40;
 
   constructor(private readonly root: HTMLElement) {
     this.init();
