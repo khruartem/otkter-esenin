9dba973561bcc788dfd1b36ec8d468a459ef7332
Artem Khrustalev <khru-artem@yandex.ru>
2026-09-09 17:35:09 +0300

    fix:поправил SCROLL_THRESHOLD контроллера browser-theme-bar

--- src/scripts/controllers/browser-theme-controller.ts
+++ src/scripts/controllers/browser-theme-controller.ts
@@ -1,5 +1,5 @@
 export class BrowserThemeController {
-  private static readonly SCROLL_THRESHOLD = 3;
+  private static readonly SCROLL_THRESHOLD = 25;
 
   constructor(private readonly root: HTMLElement) {
     this.init();
