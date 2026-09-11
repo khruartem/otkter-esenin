7e025eb06bb6d8366d44cff014c0d83536bcbe3b
Artem Khrustalev <khru-artem@yandex.ru>
2026-09-11 22:53:20 +0300

    feat:добавил миниатюры фоток для галлереи

--- src/data/photos.ts
+++ src/data/photos.ts
@@ -1,3 +1,5 @@
+import type { ImageMetadata } from "astro";
+
 import photoSource1 from "../assets/images/gallery/photo_1.avif";
 import photoSource2 from "../assets/images/gallery/photo_2.avif";
 import photoSource3 from "../assets/images/gallery/photo_3.avif";
@@ -7,27 +9,33 @@ import photoSource5 from "../assets/images/gallery/photo_5.avif";
 export type PhotoItem = {
   src: string;
   alt: string;
+  thumb: ImageMetadata;
 };
 
 export const photos: PhotoItem[] = [
   {
     src: photoSource1.src,
     alt: "Актер стоит на сценическом кубе, рядом пианистка играет на рояле",
+    thumb: photoSource1,
   },
   {
     src: photoSource2.src,
     alt: "Актер эмоционально читает стихи, сидя на сцене возле рояля",
+    thumb: photoSource2,
   },
   {
     src: photoSource3.src,
     alt: "Два актера передают друг другу чемодан во время спектакля",
+    thumb: photoSource3,
   },
   {
     src: photoSource4.src,
     alt: "Актер сидит на сцене, позади него пианистка играет на рояле",
+    thumb: photoSource4,
   },
   {
     src: photoSource5.src,
     alt: "Два актера исполняют сцену с раскинутыми руками под аккомпанемент рояля",
+    thumb: photoSource5,
   },
 ];
